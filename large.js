fe1675c761235120038aa09b15558910cdec132d
Kavya20hub <kavyaav105@gmail.com>
2025-07-19 00:04:22 +0530

    Add files via upload

---
+++ large.js
@@ -0,0 +1,24 @@
+let a=prompt("enter the number");
+let b=prompt("enter the number");
+let c=prompt("enter the number");;
+ if(a>b && a>c)
+ {
+    console.log(a, "is larger");
+
+ }
+ else if(b>a && b>c)
+ {
+    console.log(b,"is larger");
+
+ }
+ else
+ {
+    console.log(c, "is larger");
+ }
+
+ function checkEven(n)
+ {
+    return n%2==0;
+    
+ }
+ 
